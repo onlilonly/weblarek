fa0b8c60250f95f17ed0567cd9a4242ee23bd354
Uliana Golubeva <onlilonly@mail.ru>
2025-12-28 14:02:08 +0200

    Создана папка и отдельные файлы в ней под каждый класс, реализован класс со списком товаров

---
+++ src/components/base/models/catalog.ts
@@ -0,0 +1,27 @@
+import { IProduct } from "../../../types/index.ts";
+
+export class Catalog {
+    products: IProduct[];
+    selectedProduct: IProduct | null;
+
+    constructor() {
+        this.products = [];
+        this.selectedProduct = null;
+    }
+
+    setProducts(products: IProduct[]): void {
+        this.products = products;
+    }
+    getProducts(): IProduct[] {
+        return this.products;
+    }
+    getProductById(id: string): IProduct | undefined {
+        return this.products.find((product) => product.id === id);
+    }
+    setSelectedProduct(selectedProduct: IProduct): void {
+        this.selectedProduct = selectedProduct;
+    }
+    getSelectedProduct(): IProduct | null {
+        return this.selectedProduct;
+    }
+}
