73d2d5c3974264a735089115a58270b40aa7f44e
Uliana Golubeva <onlilonly@mail.ru>
2025-12-28 14:02:49 +0200

    Prettier

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,22 +1,26 @@
-export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
+export type ApiPostMethods = "POST" | "PUT" | "DELETE";
 
 export interface IApi {
     get<T extends object>(uri: string): Promise<T>;
-    post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
+    post<T extends object>(
+        uri: string,
+        data: object,
+        method?: ApiPostMethods
+    ): Promise<T>;
 }
 
 export interface IProduct {
-  id: string;
-  description: string;
-  image: string;
-  title: string;
-  category: string;
-  price: number | null;
+    id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number | null;
 }
 
 export interface IBuyer {
-  payment: 'online' | 'cash';
-  email: string;
-  phone: string;
-  address: string;
+    payment: "online" | "cash";
+    email: string;
+    phone: string;
+    address: string;
 }
